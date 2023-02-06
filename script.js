60f8136e5c0fdce37e4a8d97638dee084df6f304
AnnamTRAN <annampierretran@gmail.com>
2023-02-06 02:58:31 +0100

    interactions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*************************************************** filter **************************************************/
+/*************************************************** FILTER **************************************************/
 const tags = document.querySelectorAll('.tag')
 let slctdElements = new Set()   /*set for displayed tags only*/
 let hiddenElements  = new Set() /*set for hidden tags only*/
@@ -8,7 +8,7 @@ const filter = (event) => {
      
     if (evnt.checked){ /*checked*/
         tags.forEach(tag =>{ /*verify each post's tag if match with checkbox tag*/
-            if (evnt.value === tag.getAttribute('value')){ /*if same value add to slctedElements */
+            if (evnt.getAttribute('data-value') === tag.getAttribute('data-value')){ /*if same value add to slctedElements */
                 hiddenElements.delete(tag)
                 slctdElements.add(tag)
                 console.log(slctdElements)
@@ -16,7 +16,7 @@ const filter = (event) => {
             else if(slctdElements.has(tag)){ /*if another checkbox checked show his tags*/
                 tag.style.display = "block"
             }
-            else { /* no matching tags*/
+            else { /*no matching tags*/
                 hiddenElements.add(tag)
             }
         })
@@ -24,7 +24,7 @@ const filter = (event) => {
 
     else if(!evnt.checked){ /*unchecked*/
         tags.forEach(tag =>{ 
-            if (evnt.value === tag.getAttribute('value')){ /*if uncheck and other checkboxes checked -> hide*/
+            if (evnt.getAttribute('data-value') === tag.getAttribute('data-value')){ /*if uncheck and other checkboxes checked -> hide*/
                 slctdElements.delete(tag)
                 hiddenElements.add(tag)
                 console.log(slctdElements)
@@ -47,25 +47,117 @@ const filter = (event) => {
         hiddenElements.clear()
     }
 }
-/*************************************************** filter **************************************************/
+
+const Uncheck = () => {
+    const filters = document.querySelectorAll('input[type="checkbox"]');
+    slctdElements.clear()
+    hiddenElements.clear()
+
+    tags.forEach(tag =>{
+        tag.style.display = "block"
+    })
+
+    filters.forEach(filter =>{
+        filter.checked = false
+    }) 
+}
+/*************************************************** FILTER **************************************************/
 
 
-/*************************************************** modal **************************************************/
-const modal = document.getElementById("modal");
-const btn = document.getElementById("modal-link");
-const span = document.getElementsByClassName("close")[0];
+
+/*************************************************** POST **************************************************/
+const removeDiv = document.querySelectorAll('.remove')
+const removeButtons = document.querySelectorAll('.remove-button')
+const confirmButtons = document.querySelectorAll('button[value="yes"]')
+const cancelButtons = document.querySelectorAll('button[value="no"]')
+
+removeButtons.forEach(button =>{
+    button.addEventListener('click',()=>{
+        button.nextElementSibling.style.display = "block"
+    })
+})
+
+cancelButtons.forEach(button =>{
+    button.addEventListener('click',()=>{
+        button.closest('.remove').style.display = "none"
+    })
+})
+
+removeDiv.forEach(div =>{
+    div.addEventListener('click',(event)=>{
+        if(event.target.style.display === "block"){
+            event.target.style.display = "none"
+        }
+    })
+})
+
+confirmButtons.forEach(button =>{
+    button.addEventListener('click',()=>{
+        button.parentElement.parentElement.parentElement.parentElement.remove()
+    })
+})
+
+
+/*************************************************** POST **************************************************/
+
+
+
+/*************************************************** MODAL **************************************************/
+const modal = document.getElementById('modal')
+const btn = document.getElementById('modal-link')
+const span = document.getElementById('close')
 
 btn.onclick = function() {
-    modal.style.display = "block";
+    modal.style.display = "block"
   }
 
 span.onclick = function() {
-    modal.style.display = "none";
-
+    modal.style.display = "none"
 }
 
 window.onclick = function(event) {
     if (event.target == modal) {
-      modal.style.display = "none";
+      modal.style.display = "none"
+    }
+    else if (event.target == alert){
+        alert.style.display = "none"
+        postDiv.style.overflowY = "auto"
+        postDiv.scrollTo(0,0)
     }
 }
+
+/************** locastorage post ***************/
+const msgstore = document.getElementById('textarea')
+msgstore.value = localStorage.getItem('txt')
+
+msgstore.addEventListener('keyup',(event) =>{
+    txt = event.target.value
+    localStorage.setItem('txt',txt)
+})
+/************** locastorage post ***************/
+
+
+
+/*************************************************** MODAL **************************************************/
+
+
+
+/************************************* SCROLLING ************************/
+const postDiv = document.querySelector('#post')
+const spanScroll = document.querySelector('#close-alert');
+const alert = document.querySelector('#alert');
+
+spanScroll.onclick = function() {
+    alert.style.display = "none"
+    postDiv.style.overflowY = "auto"
+    postDiv.scrollTo(0,0)
+}
+
+postDiv.addEventListener('scroll',()=>{
+    console.log(postDiv.scrollTop)
+    if (postDiv.scrollTop> 120){
+        alert.style.display = "block"
+        postDiv.style.overflowY = "hidden"
+    }
+})
+/************************************* SCROLLING ************************/
